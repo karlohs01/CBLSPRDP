b75910001870bfea87e805677b86cc83c800d46a
karlohs <150075905+karlohs01@users.noreply.github.com>
2025-03-17 18:06:57 -0400

    Update get_system_metrics function as well as SERVER_URL path

--- agent.py
+++ agent.py
@@ -3,17 +3,17 @@ import requests
 import time
 import json
 
-# configuration 
-SERVER_URL = "https://34.139.153.229"
+SERVER_URL = "https://34.139.153.229:5000/data"
 INTERVAL = 5
 
 def get_system_metrics():
+    temperature = get_temperature()
     metrics = {
         "cpu": psutil.cpu_percent(interval=1),
         "memory": psutil.virtual_memory().percent,
         "disk": psutil.disk_usage('/').percent,
         "network": psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv,
-        "temperature": get_temperature()
+        "temperature": temperature if temperature is not None else 0.0 # ensure it is a number
     }
     return metrics
 
@@ -26,13 +26,14 @@ def get_temperature():
     except AttributeError:
         return None
     
+
 def send_data():
     while True:
-        data = get_system_metrics
+        data = get_system_metrics()
         try:
             response = requests.post(SERVER_URL, json=data, timeout=5)
             response.raise_for_status()
-
+            print(f'Data sent: {data}')
         except requests.exceptions.RequestException as e:
             print(f'Error sending data: {e}')
         time.sleep(INTERVAL)
